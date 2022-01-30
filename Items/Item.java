d35b1c7a485cc946aa137437bd6cd3248b485133
brno4758 <brno4758@colorado.edu>
2022-01-29 21:42:03 -0700

    created initial item superclass

---
+++ Items/Item.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+abstract public class Item{
+    private String name_;
+    private float purchasePrice_;
+    private float listPrice_;
+    private boolean new_;
+    private Date dayArrived_;
+    private char condition_; //Maybe we can use a custom data type here so we can have operators like -- and ++ for increasing or decreasing condition after damage
+    private float salePrice_; //Default this value to empty? Can't initialize a sale price that hasnt happened yet
+    private Date daySold_; //Default this value to current date in calander?
+
+
+
+    public void set_name(String name){name_ = name;}
+    public void set_purch_price(float price){purchasePrice_ = price;}
+    public void set_list_price(float price){listPrice_ = price;}
+    public void set_is_new(boolean isNew){new_ = isNew;}
+    public void set_day_arrived(Date arrived){dayArrived_ = arrived;}
+    public void set_condition(char cond){condition_ = cond;}
+    public void set_sale_price(float price){salePrice_ = price;}
+    public void set_day_sold(Date soldDate){daySold_ = soldDate;}
+
+    public String get_name(){return name_;}
+    public float get_purch_price(){return purchasePrice_;}
+    public float get_list_price(){return listPrice_;}
+    public boolean get_is_new(){return new_;}
+    public Date get_day_arrived(){return dayArrived_;}
+    public char get_condition(){return condition_;}
+    public float get_sale_price(){return salePrice_;}
+    public Date get_day_sold(){return daySold_;}
+    public abstract String toString();
+}
