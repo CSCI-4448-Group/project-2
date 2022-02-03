16cfe7ecbbe23820f6a9189a0dca23e6d308c063
IsaacPyle <isaac.a.pyle@gmail.com>
2022-02-02 20:08:16 -0700

    Changed dates in Calendar class from LocalDate type to int, tracking days from initial date of 0.

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,11 +1,12 @@
-import java.time.LocalDate;
-
 public class Calendar {
-    private LocalDate begin_date_;
-    private LocalDate current_date_;
+    private int begin_date_;
+    private int current_date_;
 
     public Calendar() {
-        begin_date_ = LocalDate.now();
-        current_date_ = LocalDate.now();
+        begin_date_ = 0;
+        current_date_ = 0;
     }
+
+    public int get_current_date() {return current_date_;}
+    public int get_begin_date() {return begin_date_;}
 }
